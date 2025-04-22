1a4b712b9a6a54570770359176ae4a641db41d9c
Felipe Alfaya <felipealfaya90@gmail.com>
2025-04-22 15:05:25 -0300

    adding logo to the tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type React from 'react'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -9,15 +8,30 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Facilize',
   description: 'Plataforma para encontrar e agendar serviços com profissionais',
+  icons: {
+    icon: '/images/logo-2.png',
+    shortcut: '/images/logo-2.png',
+    apple: '/images/logo-2.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/images/logo-2.png',
+    },
+  },
+  openGraph: {
+    images: '/images/logo-2.png',
+  },
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang='pt-BR' suppressHydrationWarning>
+      <head>
+        <link rel='icon' href={'/images/logo-color.svg'} sizes='any' />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute='class'
